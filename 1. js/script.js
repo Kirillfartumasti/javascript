ba94c85f97c9675ab10cf4be8684ec25b886bd4a
Kirillfartumasti <Aver-732@mail.ru>
2020-05-23 12:00:45 +0300

    первое занятие js

---
+++ 1. js/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+let age = 17;
+let salary;
+
+let x = 3;
+let y = 5;
+
+x = 3;
+
+x = x + 3;
+y = 1 + 8;
+
+
+function showMyNeme(){
+	let name = "Kirill";
+	alert(name);
+}
+
+showMyNeme();
